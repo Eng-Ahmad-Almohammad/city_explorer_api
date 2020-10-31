a5c538a4378a3a5daa35dae19641451818beafa0
Ahmad <almohammedahmed2@gmail.com>
2020-10-31 18:37:14 +0200

    movies had fixes

--- modules/movie.js
+++ modules/movie.js
@@ -9,7 +9,7 @@ function Movies(title, overview, average_votes, total_votes, image_url, populari
     this.overview = overview;
     this.average_votes = average_votes;
     this.total_votes = total_votes;
-    this.image_url = image_url;
+    this.image_url = `https://image.tmdb.org/t/p/w500${image_url}`;
     this.popularity = popularity;
     this.released_on = released_on;
 
